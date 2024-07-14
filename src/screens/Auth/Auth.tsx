3b7509f4574fc6e508ae5c34ebeaa117f6e9e701
Ilya Konoplitsky <96144903+IlyaKonoplitsky@users.noreply.github.com>
2024-07-14 20:25:43 +0500

    feat: password input

--- src/screens/Auth/Auth.tsx
+++ src/screens/Auth/Auth.tsx
@@ -1,7 +1,20 @@
-import { Typography } from '@shared'
+import { Input, Typography } from '@shared'
+
+import styles from './Auth.module.css'
 
 const Auth = () => {
-  return <Typography variant='text_36_b'>Регистрация</Typography>
+  return (
+    <div className={styles.page}>
+      <Typography className={styles.header} variant='text_36_b'>
+        Регистрация
+      </Typography>
+      <div className={styles.container}>
+        <Input label='Имя' />
+        <Input label='Email' />
+        <Input label='Пароль' type='password' />
+      </div>
+    </div>
+  )
 }
 
 export default Auth
--- src/shared/Fields/Input/Input.tsx
+++ src/shared/Fields/Input/Input.tsx
@@ -1,5 +1,7 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, useState } from 'react'
 
+import HideIcon from '@assets/icons/hide_password.svg?react'
+import ShowIcon from '@assets/icons/show_passwod.svg?react'
 import { classnames } from '@utils'
 
 import styles from './Input.module.css'
@@ -11,17 +13,43 @@ interface InputProps extends Omit<ComponentProps<'input'>, 'placeholder'> {
   className?: string
 }
 
-export const Input = ({ label, isError = false, helperText, className, ...props }: InputProps) => {
+export const Input = ({
+  label,
+  type = 'text',
+  isError = false,
+  helperText,
+  className,
+  ...props
+}: InputProps) => {
+  const [passwordVisible, setPasswordVisible] = useState(false)
+
   const containerClasses = classnames(styles.container, className)
   const stylesInput = classnames(styles.input, {
     [styles.input_error]: isError
   })
 
+  const handleTogglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible)
+  }
+
+  const inputType = type === 'password' && passwordVisible ? 'text' : type
+
   return (
     <>
       <div className={containerClasses}>
         <label className={styles.label}>{label}</label>
-        <input className={stylesInput} {...props} />
+        <div className={styles.input_wrapper}>
+          <input className={stylesInput} type={inputType} {...props} />
+          {type === 'password' && (
+            <button
+              type='button'
+              className={styles.password_toggle}
+              onClick={handleTogglePasswordVisibility}
+            >
+              {passwordVisible ? <HideIcon /> : <ShowIcon />}
+            </button>
+          )}
+        </div>
       </div>
       {isError && helperText && <div className={styles.helper_text}>{helperText}</div>}
     </>
